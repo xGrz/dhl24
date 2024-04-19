ca8c2bbd9a772dc5e4780a6e85160082010cb697
xGrz <33755434+xGrz@users.noreply.github.com>
2024-04-19 17:38:19 +0200

    Receiver address introduced

--- src/Api/Structs/Shipment/ReceiverAddress.php
+++ src/Api/Structs/Shipment/ReceiverAddress.php
@@ -2,7 +2,45 @@
 
 namespace xGrz\Dhl24\Api\Structs\Shipment;
 
-class ReceiverAddress
+class ReceiverAddress extends ShipperAddress
 {
+    public string $addressType = 'C';
+    public string $country = 'PL';
+    public ?string $isPackstation = null;
+    public ?string $isPostfiliale = null;
+    public ?string $postnummer = null;
+
+
+    public function setAddressType(string $addressType): ReceiverAddress
+    {
+        $this->addressType = $addressType;
+        return $this;
+    }
+
+    public function setCountry(string $country): ReceiverAddress
+    {
+        $this->country = $country;
+        return $this;
+    }
+
+    public function setPackStation(?string $packstation): ReceiverAddress
+    {
+        $this->isPackstation = $packstation;
+        return $this;
+    }
+
+    public function setPostfiliale(?string $postfiliale): ReceiverAddress
+    {
+        $this->isPostfiliale = $postfiliale;
+        return $this;
+    }
+
+    public function setPostnummer(?string $postnummer): ReceiverAddress
+    {
+        $this->postnummer = $postnummer;
+        return $this;
+    }
+
+
 
 }
