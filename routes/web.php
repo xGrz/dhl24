98072fe018fcea3d1efac6171d8cb14158c65f66
xGrz <33755434+xGrz@users.noreply.github.com>
2024-04-26 16:53:30 +0200

    Removed test function

--- routes/web.php
+++ routes/web.php
@@ -3,22 +3,6 @@
 use xGrz\Dhl24\Http\Controllers\CourierBookingsController;
 use xGrz\Dhl24\Http\Controllers\ShipmentsController;
 
-function array_to_xml($data, $xml = null)
-{
-    if ($xml === null) {
-        $xml = new SimpleXMLElement('<?xml version="1.0"?><root></root>');
-    }
-
-    foreach ($data as $key => $value) {
-        if (is_array($value)) {
-            array_to_xml($value, $xml->addChild(is_numeric($key) ? 'item' : $key));
-        } else {
-            $xml->addChild($key, $value);
-        }
-    }
-
-    return $xml->asXML();
-}
 
 Route::middleware(['web'])
     ->prefix('dhl')
