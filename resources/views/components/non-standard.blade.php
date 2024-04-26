13e1c0cee29dc65173cb6db8addfb213bd3ff15d
xGrz <33755434+xGrz@users.noreply.github.com>
2024-04-26 15:55:13 +0200

    Upgraded non-standard switch

--- resources/views/components/non-standard.blade.php
+++ resources/views/components/non-standard.blade.php
@@ -8,21 +8,12 @@
                 @if($model) checked @endif
             >
             <div
-                class="relative w-10 h-5 bg-slate-700 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-800
-                rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white
-                after:content-[''] after:absolute after:top-[0] after:start-[0] after:bg-white after:border-gray-300 after:border after:rounded-full after:w-5
-                after:h-5 after:transition-all peer-checked:bg-blue-600">
+                class="relative w-12 h-5 bg-slate-700 rounded-full transition-all ease-in-out duration-300 pl-0 peer-checked:pl-7 peer-focus:ring-4 peer-focus:ring-blue-800">
+                <div
+                    class="w-5 h-5 @if($shouldBeNonStandard && !$value) bg-orange-500 @else bg-white @endif rounded-full ease-in-out duration-100 transition-all"></div>
             </div>
             <span
-                class="ms-3 text-sm font-medium uppercase font-bold
-                    @if($value)
-                        text-green-500
-                    @elseif($shouldBeNonStandard)
-                        text-orange-500
-                    @else
-                        text-slate-500
-                  @endif"
-            >
+                class="ms-3 text-sm font-medium uppercase font-bold @if($value)text-green-500 @elseif($shouldBeNonStandard) text-orange-500 @else text-slate-500 @endif">
                 {{$label}}
             </span>
         </label>
